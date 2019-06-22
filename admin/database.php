04ad51e521d92200f77aa687b6a633a0c77c9585
Prashant Radadiya <prashantradadiya7@gmail.com>
2019-06-22 11:37:32 +0530

    Database Connection
    complete add database connection file  to admin panel area.

---
+++ admin/database.php
@@ -0,0 +1,22 @@
+<?php 
+
+require_once 'db_credential.php';
+
+function db_connect(){
+    $connection = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+    confrim_db_connect($connection);
+    return $connection;
+}
+
+function confrim_db_connect($connection){
+    if(!$connection){
+        $msg = "Connection is Failed : ";
+        $msg .= mysqli_connect_error();
+        $msg .= mysqli_connect_errno();
+        exit($msg);
+    } else{
+        echo "Connection is ready to use";
+    }
+}
+
+$db = db_connect();
